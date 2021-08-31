8d2fcfdfa787e28b73304dd74f908b770804280a
Tom Hartogs <tomhartogs@gmail.com>
2021-08-31 19:32:50 +0200

    [feat] Add trackEvent function

--- functions/src/Analytics/index.ts
+++ functions/src/Analytics/index.ts
@@ -1,5 +1,36 @@
 import * as functions from 'firebase-functions';
 import * as request from 'request';
+import * as urlencode from 'urlencode';
+import { WebhookRequest } from '../DialogflowCX/WebhookRequest';
+
+const GOOGLE_ANALYTICS_URL = 'https://www.google-analytics.com/collect';
+const VERSION = 'v';
+const TRACKING_ID = 'tid';
+const CLIENT_ID = 'cid';
+const HIT_TYPE = 't';
+const EVENT = 'event';
+const EVENT_CATEGORY = 'ec';
+const EVENT_ACTION = 'ea';
+const EVENT_LABEL = 'el';
+const trackEvent = (trackingId: string, userId: string, event: string, value: string, label: string) => {
+  const options = {
+    // eslint-disable-next-line max-len
+    body: `${VERSION}=1&${TRACKING_ID}=${trackingId}&${CLIENT_ID}=${userId}&${HIT_TYPE}=${EVENT}&${EVENT_CATEGORY}=${urlencode(
+      event,
+    )}&${EVENT_ACTION}=${urlencode(value)}&${EVENT_LABEL}=${urlencode(label)}`,
+    url: GOOGLE_ANALYTICS_URL,
+    json: false,
+  };
+
+  request.post(options, (error, response) => {
+    if (error) {
+      console.error(`post: ${error}`);
+      return;
+    }
+    console.log(`analytics response: ${response.statusCode}`);
+  });
+};
+
 export const analytics = functions.https.onRequest((req, res) => {
 
   res.sendStatus(200);
